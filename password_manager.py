392c3335c617f0d27bc7d9e1ddfd9ad44c8364cc
Alejandro Tovar <keatola16.ktl@gmail.com>
2021-09-08 09:09:51 -0500

    Language input by json file

--- password_manager.py
+++ password_manager.py
@@ -1,37 +1,43 @@
+import json
 import string
 
-language = 'en'
+language_var = 'en'
 
+# variable that indicates the language, default language = English
+# En = English
+# Sp = Spanish
+language = ('Spanish' if language_var == 'sp' else 'English')
+
+
+text = json.load(open('Languages/'+language+'.json', 'r'))
 # Text in different languages
-text = {'En_keyword_in1' : 'Site: ',
-'Sp_keyword_in1' : 'Sitio : ',
-'En_user' : 'Username: ',
-'Sp_user' : 'Nombre de usuario: ',
-'En_password' : 'Password: ',
-'Sp_password' : 'Contraseña: ',
-'En_corrUser': 'The user {} was creat correctly!',
-'Sp_corrUser': 'El usuario {} fue creado correctamente!',
-'En_wrongUser': 'The user {} was not created correctly, be sure the user containd just alphanumeric letters (a-z or A-Z) and (0-9), or next symbols ({}) , without spaces.',
-'Sp_wrongUser': 'El usuario {} no se creó correctamente, verifique que contenga solo letras mayusculas o minusculas (a-z o A-Z), numeros (0-9) ó los siguientes simbolos ({}), sin espacios',
-'En_wrongpass': 'The password was not created correctly, be sure the user containd just alphanumeric letters (a-z or A-Z) and (0-9), or next symbols ({}) , without spaces.',
-'Sp_wrongpass': 'La contraseña no se creó correctamente, verifique que contenga solo letras mayusculas o minusculas (a-z o A-Z), numeros (0-9) ó los siguientes simbolos ({}), sin espacios',
-'En_passAgain': 'Write a correct password: ',
-'Sp_passAgain': 'Escriba una contraseña correcta: ',
-'En_keyAgain': 'Write a correct site name: ',
-'Sp_keyAgain': 'Escriba el nombre del sitio de forma correcta: ',
-'En_userAgain': 'Write a correct username: ',
-'Sp_userAgain': 'Escriba el nombre de usuario del sitio de forma correcta: ',
-'En_shortPass': 'The password is too short and weakness, try with minimum 6 characters',
-'Sp_shortPass': 'La contraseña es muy corta y debil, intente con una longitud minima de 6 caracteres',
+
+# English = {'keyword_in1' : 'Site: ',
+# 'user' : 'Username: ',
+# 'password' : 'Password: ',
+# 'corrUser': 'The user {} was creat correctly!',
+# 'wrongUser': 'The user {} was not created correctly, be sure the user containd just alphanumeric letters (a-z or A-Z) and (0-9), or next symbols ({}) , without spaces.',
+# 'wrongpass': 'The password was not created correctly, be sure the user containd just alphanumeric letters (a-z or A-Z) and (0-9), or next symbols ({}) , without spaces.',
+# 'passAgain': 'Write a correct password: ',
+# 'keyAgain': 'Write a correct site name: ',
+# 'userAgain': 'Write a correct username: ',
+# 'shortPass': 'The password is too short and weakness, try with minimum 6 characters',}
+
+Spanish = {'keyword_in1' : 'Sitio : ',
+'user' : 'Nombre de usuario: ',
+'password' : 'Contraseña: ',
+'corrUser': 'El usuario {} fue creado correctamente!',
+'wrongUser': 'El usuario {} no se creó correctamente, verifique que contenga solo letras mayusculas o minusculas (a-z o A-Z), numeros (0-9) ó los siguientes simbolos ({}), sin espacios',
+'wrongpass': 'La contraseña no se creó correctamente, verifique que contenga solo letras mayusculas o minusculas (a-z o A-Z), numeros (0-9) ó los siguientes simbolos ({}), sin espacios',
+'passAgain': 'Escriba una contraseña correcta: ',
+'keyAgain': 'Escriba el nombre del sitio de forma correcta: ',
+'userAgain': 'Escriba el nombre de usuario del sitio de forma correcta: ',
+'shortPass': 'La contraseña es muy corta y debil, intente con una longitud minima de 6 caracteres',
 }
 
 # Permited characters
 perm_chars = list(string.ascii_letters+ string.digits+ 'ñÑ'+ string.punctuation)
 
-# variable that indicates the language, default language = English
-# En = English
-# Sp = Spanish
-lang_var = ('Sp_' if language == 'sp' else 'En_')
 # __________________________________________________________________________________________________
 
 def verificate_password(password):
@@ -42,12 +48,12 @@ def verificate_password(password):
         for letter in password:
                 if letter in perm_chars: pass
                 else:
-                    print (text[lang_var + 'wrongpass'].format(string.punctuation))
+                    print (text['wrongpass'].format(string.punctuation))
                     _, New_password = input_var(0,0,1,'','','passAgain')
 
     else:
         err_status = 1
-        print(text[lang_var + 'shortPass'])
+        print(text['shortPass'])
 
 # __________________________________________________________________________________________________
 def input_var(keyword, user,password ,key_text, user_text,pas_text):
@@ -68,24 +74,24 @@ def input_var(keyword, user,password ,key_text, user_text,pas_text):
     New_password: the new password entered by the user
     """
     if keyword == 1:
-        New_keyword = str(input(text[lang_var + key_text])).capitalize()
+        New_keyword = input(text[key_text]).capitalize()
 
         for letter in New_keyword:
             if letter in perm_chars: pass
             else:
-                print (text[lang_var + 'wrongUser'].format(New_keyword, string.punctuation))
+                print (text['wrongUser'].format(New_keyword, string.punctuation))
                 New_keyword, _ = input_var(1,0,0,'keyAgain','','')
     else: New_keyword = ''
 
     if user ==1:
-        New_user = input(text[lang_var + user_text])
+        New_user = input(text[user_text])
         if letter in perm_chars: pass
         else:
-            print (text[lang_var + 'wrongUser'].format(New_keyword, string.punctuation))
+            print (text['wrongUser'].format(New_keyword, string.punctuation))
             New_keyword, _ = input_var(0,1,0, '','userAgain','')
 
     if password == 1:
-        New_password = input(text[lang_var + pas_text])
+        New_password = input(text[pas_text])
 
 
     else: New_password = ''
@@ -94,8 +100,8 @@ def input_var(keyword, user,password ,key_text, user_text,pas_text):
 # __________________________________________________________________________________________________
 def sign_up():
     # Creates a new user
-    New_User = input(lang_var + 'user')
-    Password = input(lang_var+'password')
+    New_User = input('user')
+    Password = input('password')
 
     if New_User.isidentifier():
         print('')
