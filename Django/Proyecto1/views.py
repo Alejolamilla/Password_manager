cc6d70a28ac9086792ef960ddcb69191f3b493d7
simio <yugipoi@gmail.com>
2021-10-05 10:20:21 -0500

    añadiendo css

--- Django/Proyecto1/views.py
+++ Django/Proyecto1/views.py
@@ -1,4 +1,12 @@
+from os import close
 from django.http import HttpResponse
+from django.shortcuts import render
+
+import datetime
 
 def main(request):
-    return HttpResponse("XD")
+
+    date = datetime.datetime.now()
+    dic = {"fecha":date}
+
+    return render(request,"index.html",dic)
